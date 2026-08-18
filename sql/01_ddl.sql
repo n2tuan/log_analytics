cc9547d1b490b93838d6ebbe8a247cd0967730ab
TuanNN <tuannn@yahoo.com>
2026-08-18 09:11:08 +0700

    Create DB command

--- sql/01_ddl.sql
+++ sql/01_ddl.sql
@@ -1,4 +1,5 @@
 --duckdb duckdb/log_analytics.duckdb < sql/01_ddl.sql 
+CREATE OR REPLACE DATABASE log_analytics;
 CREATE SCHEMA IF NOT EXISTS raw;
 CREATE SCHEMA IF NOT EXISTS log_staging;
 CREATE SCHEMA IF NOT EXISTS log_mart;
